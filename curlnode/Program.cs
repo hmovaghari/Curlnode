abf1e9372fb348b760d083bf1d824493c9f12543
s.maskanati <Program-1@TVS-2>
2024-05-06 16:24:02 +0330

    Add project files.

---
+++ curlnode/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+
+namespace curlnode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //args = new string[] { "https://hmovaghari.ir/root/ip.js" };
+            if ((args?.Count() ?? 0) == 0 || args.Count() > 1)
+            {
+                Console.WriteLine("curlnode: try 'curlnode --help' for more information");
+            }
+            else if (args[0] == "--help")
+            {
+                Console.WriteLine("To run, node must be installed on the system");
+                Console.WriteLine("curlnode: try 'curlnode http://example.com/file.js");
+                Console.WriteLine("curlnode: try 'curlnode file:///C:/example/file.js'");
+            }
+            else
+            {
+                try
+                {
+                    var command = "/c curl -s " + args[0] + @"| node -e ""let data = ''; process.stdin.on('data', chunk => { data += chunk; }); process.stdin.on('end', () => { eval(data); }); """;
+                    //Process.Start("CMD.exe", command);
+
+                    Process process= new Process();
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.RedirectStandardOutput = true;
+                    process.StartInfo.FileName = "CMD.exe";
+                    process.StartInfo.Arguments = command;
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    Console.WriteLine(output);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Error: {exception.Message}");
+                }
+            }
+        }
+    }
+}
